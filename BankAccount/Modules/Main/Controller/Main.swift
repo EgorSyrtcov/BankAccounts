4d8e5ffc52e86a695d1c4a94bab363a0c037a927
Egor Syrtcov <sev_84@inbox.ru>
2020-03-17 10:37:18 +0300

    Main Screen

--- BankAccount/Modules/Main/Controller/Main.swift
+++ BankAccount/Modules/Main/Controller/Main.swift
@@ -8,11 +8,65 @@
 
 import UIKit
 
-class Main: UIViewController {
+enum TabBarType: String {
+    case time
+    case message
+    case account
+    case close
+}
 
+class Main: UIViewController {
+    
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var tabBarView: UIView!
+    @IBOutlet weak var currentBalanceView: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    private let cornerRadius: CGFloat = 20
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        setupTabBarView()
     }
+    
+    @IBAction func pressedButton(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            print(TabBarType.time)
+        case 1:
+            print(TabBarType.message)
+        case 2:
+            print(TabBarType.account)
+        case 3:
+            print(TabBarType.close)
+        default:
+            return
+        }
+    }
+    
+    private func setupTabBarView() {
+        tabBarView.layer.cornerRadius = cornerRadius
+        currentBalanceView.layer.cornerRadius = cornerRadius
+    }
+}
+
+extension Main: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        cell.moneyLabel.text = "-$120.9\(indexPath.row)"
+        cell.onlineLabel.text = "Online"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    
 }
 
---
+++ BankAccount/Modules/Main/View/TableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  TableViewCell.swift
+//  BankAccount
+//
+//  Created by Egor Syrtcov on 3/17/20.
+//  Copyright © 2020 Egor Syrtcov. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var mainImage: UIImageView! {
+        didSet {
+            self.mainImage.layer.cornerRadius = 40
+        }
+    }
+    @IBOutlet weak var onlineLabel: UILabel!
+    @IBOutlet weak var timeDayLabel: UILabel!
+    @IBOutlet weak var moneyLabel: UILabel!
+    
+}
