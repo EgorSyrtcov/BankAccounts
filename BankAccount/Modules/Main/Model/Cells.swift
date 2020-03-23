29424d658822fce1cbd92e207296f94bde64c49c
Egor Syrtcov <sev_84@inbox.ru>
2020-03-23 17:39:11 +0300

    fix cells model

---
+++ BankAccount/Modules/Main/Model/Cells.swift
@@ -0,0 +1,40 @@
+//
+//  Cells1.swift
+//  BankAccount
+//
+//  Created by Egor Syrtcov on 3/23/20.
+//  Copyright © 2020 Egor Syrtcov. All rights reserved.
+//
+
+import ObjectMapper
+
+struct Cells: Codable  {
+    let type: String?
+    let balance: Int?
+    let date: Int?
+    let icon: String?
+    let sum: Int?
+    let title: String?
+}
+
+extension Cells: ImmutableMappable {
+    
+    init(map: Map) throws {
+        type = try map.value("type")
+        balance = try map.value("balance")
+        date = try map.value("date")
+        icon = try map.value("icon")
+        sum = try map.value("sum")
+        title = try map.value("title")
+    }
+    
+    func mapping(map: Map) {
+        type >>> map["type"]
+        balance >>> map["balance"]
+        date >>> map["date"]
+        icon >>> map["icon"]
+        sum >>> map["sum"]
+        title >>> map["title"]
+    }
+}
+
